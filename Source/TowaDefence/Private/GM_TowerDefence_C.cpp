bc02cf508ff70cb648f49e5d0e275ef3e9447456
Erikos <Erikos66@outlook.com>
2024-12-12 11:35:23 +0000

    Initial Commit And Push

---
+++ Source/TowaDefence/Private/GM_TowerDefence_C.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+// Test for Commit
+// New Test For commit
+//Final Test
+
+#include "GM_TowerDefence_C.h"
+
---
+++ Source/TowaDefence/Public/GM_TowerDefence_C.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GM_TowerDefence_C.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWADEFENCE_API AGM_TowerDefence_C : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
