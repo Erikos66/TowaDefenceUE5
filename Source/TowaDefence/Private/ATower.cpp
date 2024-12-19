a7cbb0f03e2e6b1741f8761edf4f76247069e8f0
Erikos <Erikos66@outlook.com>
2024-12-19 08:36:10 +0000

    The C++ing of towers

---
+++ Source/TowaDefence/Private/ATower.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "Components/ArrowComponent.h"
+#include "ATower.h"
+
+
+
+AATower::AATower()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	TowerMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TowerMesh"));
+	RootComponent = TowerMesh;
+
+	CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionSphere"));
+	CollisionSphere->SetupAttachment(RootComponent);
+
+	ArrowComponent = CreateDefaultSubobject<UArrowComponent>(TEXT("ArrowComponent"));
+	ArrowComponent->SetupAttachment(RootComponent);
+}
+
+void AATower::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AATower::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/TowaDefence/Public/ATower.h
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ATower.generated.h"
+
+class UStaticMeshComponent;
+class USphereComponent;
+class UArrowComponent;
+
+UCLASS()
+class TOWADEFENCE_API AATower : public AActor
+{
+	GENERATED_BODY()
+
+public:
+
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// PROPERTIES & VARIABLES
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tower")
+	UStaticMeshComponent* TowerMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tower")
+	USphereComponent* CollisionSphere;
+	
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// FUNCTIONS
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+protected:
+
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// PROPERTIES & VARIABLES
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tower")
+	UArrowComponent* ArrowComponent;
+
+	float CurrentHealth;
+	float MaxHealth;
+	float Damage;
+	float AttackSpeed;
+	float AttackRange;
+	float AttackTimer;
+	float RotationSpeed;
+	int32 CostValue;
+	int32 SellValue;
+	int32 UpgradeCost;
+	int32 CurrentLevel;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tower")
+	TArray<AActor*> TargetArray;
+	
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// FUNCTIONS
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+	AATower();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+	
+};
